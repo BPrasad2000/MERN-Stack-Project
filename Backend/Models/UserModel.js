d01699f1234587fc95f8eb0828ae968e217c1d52
BPrasad2000 <buddhiprasad0784558@gmail.com>
2025-03-27 11:28:28 +0530

    create user model

---
+++ Backend/Models/UserModel.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const Schema =mongoose.Schema;
+
+const userSchema = new Schema({
+    name:{
+        type:String,
+        required:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+    phone:{
+        type:Number,
+        required:true,
+    },
+    address:{
+        type:String,
+        required:true,
+    },
+    role:{
+        type:Number,
+        default:0,
+    }
+
+},{timestamps:true});
+
+export default mongoose.model('users',userSchema)
+
