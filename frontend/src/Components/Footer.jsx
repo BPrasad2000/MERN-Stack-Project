79ae13b9a500990e7c9f87211473ad5528170458
BPrasad2000 <buddhiprasad0784558@gmail.com>
2025-04-04 10:51:23 +0530

    update footer

--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className="bg-black text-white p-3">
+       <h4 className="text-center pt-2">All Right Reserved &copy; Prasad</h4>
+    </div>
   )
 }
 
